09ffe18340a7fd0999145c0092a4acf83acb7134
Sshashank18 <shashankaggarwal13@gmail.com>
2021-12-09 18:55:45 +0530

    Updates

---
+++ precache-manifest.84cd216a8181e3f138973853517a2ab1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7837917e2b7f38c02d8e2b0eaef318b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f84c799c04577f965894",
+    "url": "/Portfolio/static/css/main.c3026aae.chunk.css"
+  },
+  {
+    "revision": "8ef9f2026217bbee3f6a",
+    "url": "/Portfolio/static/js/2.f02ee89d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.f02ee89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84c799c04577f965894",
+    "url": "/Portfolio/static/js/main.c9fdc1a4.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5c764e174fcaaa0830a1cc78e122f813",
+    "url": "/Portfolio/static/media/back.5c764e17.jpg"
+  },
+  {
+    "revision": "ba73b063d4752b5ef0f68fa56d8e1c99",
+    "url": "/Portfolio/static/media/image.ba73b063.png"
+  },
+  {
+    "revision": "8bf78ed9c4fcbeaed077599402b6213e",
+    "url": "/Portfolio/static/media/loading.8bf78ed9.gif"
+  },
+  {
+    "revision": "afcc2c006224cc2d2f9e4b9c4c4196da",
+    "url": "/Portfolio/static/media/mountains.afcc2c00.jpg"
+  },
+  {
+    "revision": "16cc06ca209593c023130099d81cc3bf",
+    "url": "/Portfolio/static/media/s_logo.16cc06ca.png"
+  }
+]);
